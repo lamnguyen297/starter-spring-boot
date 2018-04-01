661c2f845f04a5896c48d5da70b45c2b44b74922
lamnguyen297 <thanhlam297@gmail.com>
2018-04-01 20:30:35 +0700

    init project

---
+++ src/test/java/com/goodii/starterspringboot/StarterSpringBootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.goodii.starterspringboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StarterSpringBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
